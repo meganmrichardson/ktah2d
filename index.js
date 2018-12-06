a54c452426c7d87a4cd8aa7172b4172747f92e82
Megan Richardson <41877902+meganmrichardson@users.noreply.github.com>
2018-12-06 14:35:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function drawHealthBar() {
   if (playerHealth > 0) {
     fill(playerHealth > 25 ? "green" : "red");
     rect(640, 10, 1.5 * playerHealth, 10);
-  }
-  else if (playerHealth <= 0) {
+  } else if (playerHealth <= 0) {
     fill("rgba(100, 100, 100, 0.5)");
     rect(0, 0, width, height);
     textFont("Avenir");
@@ -17,10 +16,39 @@ function drawHealthBar() {
     textSize(40);
     textStyle(BOLD);
     fill(0);
-    text("GAME OVER", width/2 -20, height/2 - 20);
-    text(`${Math.round(time)} seconds`, width/2 -20, height/2 + 20);
-    player.speed = 0;
-    enemy.speed = 0;
+    text("GAME OVER", width / 2 - 20, height / 2 - 20);
+    text(`${Math.round(time)} seconds`, width / 2 - 20, height / 2 + 20);
+    exit();
+  }
+}
+
+function updateTimer() {
+  fill("grey");
+  time += 1 / 60;
+  textSize(30);
+  textAlign(RIGHT);
+  text(Math.round(time), width - 10, 50);
+}
+
+function updateScarecrow() {
+  fill("grey");
+  rect(10, 10, 150, 10);
+  if (scarecrow) {
+    scarecrow.draw();
+    scarecrow.ttl--;
+    if (scarecrow.ttl < 0) {
+      scarecrow = undefined;
+      cooldown = time;
+    }
+  } else {
+    fill("white");
+    rect(10, 10, time - cooldown < 10 ? 15 * (time - cooldown) : 150, 10);
+    if (time - cooldown > 10) {
+      fill("grey");
+      textSize(15);
+      textAlign(LEFT);
+      text("Click to use bait!", 10, 40);
+    }
   }
 }
 
@@ -31,7 +59,14 @@ class mainCharacter {
   draw() {
     fill(this.color);
     ellipse(this.x, this.y, this.radius * 2, this.radius * 1.8);
-    triangle(this.x, this.y, this.x + 15, this.y - 10, this.x + 15, this.y +10);
+    triangle(
+      this.x,
+      this.y,
+      this.x + 15,
+      this.y - 10,
+      this.x + 15,
+      this.y + 10
+    );
     fill("black");
     ellipse(this.x - 3, this.y - 3, this.radius / 3, this.radius / 3);
   }
@@ -41,21 +76,95 @@ class mainCharacter {
   }
 }
 
-class enemyCharacter {
-  constructor(x, y, radius, speed) {
-    Object.assign(this, { x, y, radius, speed });
+class enemy {
+  constructor(type, x, y, radius, speed) {
+    Object.assign(this, { type, x, y, radius, speed });
   }
   draw() {
-    fill("rgb(160, 200, 255)");
-    ellipse(this.x, this.y, this.radius * 2, this.radius);
-    triangle(this.x - 8, this.y - 10, this.x + 5, this.y - 30, this.x + 7, this.y - 2);
-    triangle(this.x + 40, this.y - 30, this.x + 10, this.y, this.x + 30, this.y);
-    triangle(this.x + 40, this.y + 20, this.x + 10, this.y, this.x + 30, this.y);    
-    triangle(this.x - 8, this.y + 10, this.x + 5, this.y + 30, this.x + 7, this.y + 2);
-    fill("black");
-    rect(this.x - 30, this.y + 3, 15, 1);
-    ellipse(this. x - 15, this.y - 3, this.radius / 8)
-    fill("white");
+    if (this.type === "shark") {
+      fill("rgb(160, 200, 255)");
+      ellipse(this.x, this.y, this.radius * 2, this.radius);
+      triangle(
+        this.x - 8,
+        this.y - 10,
+        this.x + 5,
+        this.y - 30,
+        this.x + 7,
+        this.y - 2
+      );
+      triangle(
+        this.x + 40,
+        this.y - 30,
+        this.x + 10,
+        this.y,
+        this.x + 30,
+        this.y
+      );
+      triangle(
+        this.x + 40,
+        this.y + 20,
+        this.x + 10,
+        this.y,
+        this.x + 30,
+        this.y
+      );
+      triangle(
+        this.x - 8,
+        this.y + 10,
+        this.x + 5,
+        this.y + 30,
+        this.x + 7,
+        this.y + 2
+      );
+      fill("black");
+      rect(this.x - 30, this.y + 3, 15, 1);
+      ellipse(this.x - 15, this.y - 3, this.radius / 8);
+    } else if (this.type === "starfish") {
+      fill("rgb(255, 130, 120)");
+      star(this.x, this.y, this.radius, this.radius / 2, 5);
+      function star(x, y, outerRadius, innerRadius, points) {
+        let angle = TWO_PI / points;
+        let halfAngle = angle / 2.0;
+        beginShape();
+        for (let a = 0; a < TWO_PI; a += angle) {
+          let sx = x + cos(a) * innerRadius;
+          let sy = y + sin(a) * innerRadius;
+          vertex(sx, sy);
+          sx = x + cos(a + halfAngle) * outerRadius;
+          sy = y + sin(a + halfAngle) * outerRadius;
+          vertex(sx, sy);
+        }
+        endShape(CLOSE);
+      }
+      fill("black");
+      ellipse(this.x - 4, this.y - 1, this.radius / 5);
+      ellipse(this.x + 2, this.y - 3, this.radius / 5);
+    } else if (this.type === "jellyfish") {
+      fill("rgb(220, 120, 255)");
+      ellipse(this.x, this.y, this.radius * 2, this.radius);
+      rect(this.x + 2, this.y, 2, this.radius * 1.6);
+      rect(this.x - 3, this.y, 2, this.radius * 1.4);
+      rect(this.x + 7, this.y, 2, this.radius * 1.4);
+      rect(this.x + 12, this.y, 2, this.radius * 1.6);
+      rect(this.x - 8, this.y, 2, this.radius * 1.6);
+      rect(this.x - 13, this.y, 2, this.radius * 1.4);
+      fill("black");
+      ellipse(this.x - 4, this.y + 2, this.radius / 5);
+      ellipse(this.x + 4, this.y + 2, this.radius / 5);
+    } else if (this.type === "fish") {
+      fill("red");
+      ellipse(this.x, this.y, this.radius * 2, this.radius * 1.8);
+      triangle(
+        this.x,
+        this.y,
+        this.x + 15,
+        this.y - 10,
+        this.x + 15,
+        this.y + 10
+      );
+      fill("black");
+      ellipse(this.x - 3, this.y - 3, this.radius / 3, this.radius / 3);
+    }
   }
   move(target) {
     this.x += (target.x - this.x) * this.speed;
@@ -64,18 +173,20 @@ class enemyCharacter {
 }
 
 const player = new mainCharacter(100, 30, "orange", 10, 0.05);
-const enemies = [];
+let enemies = [];
 let scarecrow;
+let cooldown = 0;
 
-function addShark(
+function addEnemy(
+  type,
   amount = 1,
   coordinates = [Math.random() * width, Math.random() * height],
-  size = Math.random() * 20 + 10,
+  size = 30,
   speed = Math.random() * 0.03 + 0.003
 ) {
   for (let i = 0; i < amount; i++) {
     enemies.push(
-      new enemyCharacter(coordinates[0] + i, coordinates[1], size, speed)
+      new enemy(type, coordinates[0] + i, coordinates[1], size, speed)
     );
   }
 }
@@ -83,10 +194,10 @@ function addShark(
 function setup() {
   createCanvas(800, 600);
   noStroke();
-  // addEnemy();
-  // addEnemy(3);
-  // addEnemy(4);
-  addShark(3, [300, 300], 30, 0.01);
+  addEnemy("fish", 1, [400, 400], 10, 0.05);
+  addEnemy("shark", 1, [300, 300], 30, 0.01);
+  addEnemy("starfish", 3, [100, 100], 15, 0.03);
+  addEnemy("jellyfish", 1, [500, 500], 15, 0.035);
 }
 
 function draw() {
@@ -95,18 +206,16 @@ function draw() {
   player.move({ x: mouseX, y: mouseY });
   enemies.forEach(enemy => enemy.draw());
   enemies.forEach(enemy => enemy.move(scarecrow || player));
-  if (scarecrow) {
-    scarecrow.draw();
-    scarecrow.ttl--; 
-    if (scarecrow.ttl < 0) {
-      scarecrow = undefined;
-    }
-  }
   adjust();
   drawHealthBar();
-  time += 1/60;
-  textSize(30);
-  text(Math.round(time), width-40, 50);
+  updateTimer();
+  updateScarecrow();
+  if ((Math.round(60 * time) / 60) % 10 === 0) {
+    addEnemy("fish", 1, undefined, 10);
+    addEnemy("shark", 1, undefined, 30);
+    addEnemy("starfish");
+    addEnemy("jellyfish", 1, undefined, 15);
+  }
 }
 
 function adjust() {
@@ -132,15 +241,15 @@ function pushOff(c1, c2) {
     if (c1 === player) {
       playerHealth -= 0.1;
       if (playerHealth == 0) {
-        fill('red');
-        rect(100,100,100,100);
+        fill("red");
+        rect(100, 100, 100, 100);
       }
     }
   }
 }
 
 function mouseClicked() {
-  if (!scarecrow) {
+  if (!scarecrow & (time - cooldown > 10)) {
     scarecrow = new mainCharacter(player.x, player.y, "white", 10, 0);
     scarecrow.ttl = frameRate() * 5;
   }
